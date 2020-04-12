57aac3a2b93c149fcf05524cb8a3847de09667ea
Ari Tserendavaa <tserenda@hotmail.com>
2020-04-11 20:58:45 -0700

    single page application

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,25 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Menu from "./Menu";
 import DISHES from "../dishes";
+import PROMOTIONS from "../promotions";
+import LEADERS from "../leaders";
 import Header from "./Header";
 import Footer from "./Footer";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./Home";
+import Contact from "./Contact";
 
 function Main() {
-  const [dishes, setDishes] = useState([]);
+  const [dishes, setDishes] = useState(DISHES);
+  const [promotions, setPromotions] = useState(PROMOTIONS);
+  const [leaders, setLeaders] = useState(LEADERS);
 
-  useEffect(() => {
-    setDishes(DISHES);
-  }, []);
-
-  const HomePage = () => <Home />;
+  const HomePage = () => {
+    console.log("Dishes", dishes);
+    return (
+      <Home
+        dish={dishes.filter((dish) => dish.featured)[0]}
+        promotion={promotions.filter((promotion) => promotion.featured)[0]}
+        leader={leaders.filter((leader) => leader.featured)[0]}
+      />
+    );
+  };
 
   return (
     <div>
       <Header />
       <Switch>
         <Route path="/home" component={HomePage} />
+        <Route exact path="/contact" component={Contact} />
         <Route exact path="/menu" component={() => <Menu dishes={dishes} />} />
         <Redirect to="/home" />
       </Switch>
