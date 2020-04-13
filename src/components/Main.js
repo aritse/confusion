b88c87ba29b0b3b2d19d8001a1531e18fedd4079
Ari Tserendavaa <tserenda@hotmail.com>
2020-04-12 21:42:02 -0700

    redux thunk

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Menu from "./Menu";
 import Header from "./Header";
 import Footer from "./Footer";
@@ -8,7 +8,7 @@ import Contact from "./Contact";
 import About from "./About";
 import DishDetail from "../components/DishDetail";
 import { connect } from "react-redux";
-import { addComment } from "../redux/ActionCreators";
+import { addComment, fetchDishes } from "../redux/ActionCreators";
 
 const mapStateToProps = (state) => {
   return {
@@ -21,23 +21,34 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => ({
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+  fetchDishes: () => {
+    dispatch(fetchDishes());
+  },
 });
 
 function Main(props) {
   const HomePage = () => {
     return (
       <Home
-        dish={props.dishes.filter((dish) => dish.featured)[0]}
+        dish={props.dishes.dishes.filter((dish) => dish.featured)[0]}
+        dishesLoading={props.dishes.isLoading}
+        dishesErrMess={props.dishes.errMess}
         promotion={props.promotions.filter((promotion) => promotion.featured)[0]}
         leader={props.leaders.filter((leader) => leader.featured)[0]}
       />
     );
   };
 
+  useEffect(() => {
+    props.fetchDishes();
+  }, []);
+
   const DishWithId = ({ match }) => {
     return (
       <DishDetail
-        dish={props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+        dish={props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+        isLoading={props.dishes.isLoading}
+        errMess={props.dishes.errMess}
         comments={props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
         addComment={props.addComment}
       />
